d103abf30126c9e43e7cf977bdeb245964d7d46b
wushuailei <wushuailei@aspirecn.com>
2023-10-23 14:50:49 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,53 @@
+<template>
+	<view class="content">
+		<TnTabbar
+		    v-model="currentTabbar"
+		    fixed
+		  >
+		    <TnTabbarItem
+		      v-for="(item, index) in tabbarData"
+		      :key="index"
+		      :icon="item.icon"
+		      :active-icon="item.activeIcon"
+		      :text="item.name"
+		    />
+		  </TnTabbar>
+	</view>
+</template>
+
+<script setup>
+	// 首页
+	import TnTabbar from '@/uni_modules/tuniaoui-vue3/components/tabbar/src/tabbar.vue'
+	import TnTabbarItem from '@/uni_modules/tuniaoui-vue3/components/tabbar/src/tabbar-item.vue'
+	import { ref } from 'vue'
+	
+	const currentTabbar = ref(0)
+	
+	// 导航栏数据
+	const tabbarData = [
+	  {
+	    name: '消息',
+	    icon: 'home',
+	    activeIcon: 'home-fill',
+	  },
+	  {
+	    name: '通讯录',
+	    icon: 'like',
+	    activeIcon: 'like-fill',
+	  },
+	  {
+	    name: '发现',
+	    icon: 'fire',
+	    activeIcon: 'fire-fill',
+	  },
+	  {
+	    name: '我',
+	    icon: 'my-circle',
+	    activeIcon: 'my-circle-fill',
+	  },
+	]
+	
+</script>
+
+<style lang="scss" scoped>
+</style>
